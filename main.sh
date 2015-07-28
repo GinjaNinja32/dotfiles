215598da12c8af8fab0f0667c5f06631dbc639e9
GinjaNinja32 <ginjaninja32@gmail.com>
2015-07-28 18:35:01 +0100

    Reordering

--- main.sh
+++ main.sh
@@ -9,7 +9,7 @@ srcifex() {
 srcifex "$HOME/byond/setup.sh"
 
 srcifex "$HOME/dotfiles/colordefs.sh"
-srcifex "$HOME/dotfiles/aliases.sh"
 srcifex "$HOME/dotfiles/misc.sh"
+srcifex "$HOME/dotfiles/aliases.sh"
 srcifex "$HOME/dotfiles/local.sh"
 true
