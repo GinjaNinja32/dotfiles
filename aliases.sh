b59eedf9e1a3ddb6d6bd7fa44cceb9fed510ea40
GinjaNinja32 <ginjaninja32@gmail.com>
2015-09-03 14:30:33 +0100

    dircolors

--- aliases.sh
+++ aliases.sh
@@ -2,11 +2,16 @@
 #! /bin/bash
 
 if [ -x /usr/bin/dircolors ]; then
-        test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-        alias ls='ls --color=auto'
-        alias grep='grep --color=auto'
-        alias fgrep='fgrep --color=auto'
-        alias egrep='egrep --color=auto'
+	if [[ "$TERM" =~ 256 ]]; then
+		dircolfile=$HOME/dotfiles/dircolors/256
+	else
+		dircolfile=$HOME/dotfiles/dircolors/16
+	fi
+	test -r $dircolfile && eval "$(dircolors -b $dircolfile)" || eval "$(dircolors -b)"
+	alias ls='ls --color=auto'
+	alias grep='grep --color=auto'
+	alias fgrep='fgrep --color=auto'
+	alias egrep='egrep --color=auto'
 fi
 
 alias lh='ls -sh'
