21117e7adc45bde7a494c8ded85d3f228393f289
GinjaNinja32 <ginjaninja32@gmail.com>
2016-09-25 14:24:26 +0100

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,44 @@
+
+if ! which git > /dev/null; then
+	echo "install git before running this!"
+	exit 1
+fi
+
+linkit() {
+	[[ -e "$1" ]] && mv "$1" "$1.initial"
+	ln -s "dotfiles/$2" "$1"
+}
+
+# Config files
+linkit ~/.tmux.conf tmux.conf
+linkit ~/.bashrc main.sh
+linkit ~/.profile profile
+linkit ~/.Xresources Xresources
+
+if [[ -e ~/.i3 ]]; then
+	linkit ~/.i3/config i3_config
+	linkit ~/.i3status.conf i3status.conf
+fi
+
+# Git prompt
+mkdir -p ~/bin/includes
+curl -o ~/bin/includes/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+# git stuff start
+mkdir ~/git
+cd ~/git
+
+# ssh-ident
+git clone https://github.com/ccontavalli/ssh-ident
+(cd ssh-ident; git remote set-url origin git@github.com:ccontavalli/ssh-ident)
+linkit ~/bin/ssh ~/git/ssh-ident/ssh-ident
+linkit ~/bin/ssh-ident ~/git/ssh-ident/ssh-ident
+
+# nanorc
+git clone https://github.com/GinjaNinja32/nanorc
+(cd nanorc; git remote set-url origin git@github.com:GinjaNinja32/nanorc)
+(cd nanorc; make install)
+linkit ~/.nanorc ~/.nano/syntax/ALL.nanorc
+
+# git stuff end
+cd ..
