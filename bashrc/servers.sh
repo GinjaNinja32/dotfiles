34f42ecb0dbd216d6c7387497dc4ef58d4d02e97
GinjaNinja32 <ginjaninja32@gmail.com>
2016-12-07 18:38:43 +0000

    use ssh config for hosts instead of hardcoding it all in servers.sh

--- bashrc/servers.sh
+++ bashrc/servers.sh
@@ -1,16 +1,10 @@
 
 setup-server() {
-	hostname=$1
-	command=$2
-	address=$3
-
-	if [[ $(hostname -s) != $hostname ]]; then
-		alias $command="mosh $address"
-	fi
+	alias $1="mosh $1"
 }
 
-setup-server nyx nyx nyx.gn32.uk
-setup-server nyx bot32 bot32@nyx.gn32.uk
-setup-server erebus erebus erebus.gn32.uk
-setup-server tyche tyche tyche.gn32.uk
-setup-server themis themis themis.gn32.uk
+setup-server nyx
+setup-server bot32
+setup-server erebus
+setup-server tyche
+setup-server themis
