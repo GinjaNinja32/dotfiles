8be392adcf8169aec95d8029275fb68bc64f112d
Ben Paxton <ben.paxton@headforwards.com>
2017-09-18 15:08:35 +0100

    Add docker2tar, update xcompose

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -52,6 +52,18 @@ alias drm='docker rm -fv $(docker ps -qa)'
 alias dvrm='docker volume rm $(docker volume ls -q)'
 alias dll='docker logs $(docker ps -a | head -2 | tail -1 | awk '\''{print $1}'\'')'
 
+export COMP_WORDBREAKS=$(echo "$COMP_WORDBREAKS" | sed 's|:||g')
+docker2tar() {
+	id=$(docker create $1)
+	docker export $id
+	docker rm -fv $id >/dev/null
+}
+_docker2tar() {
+	local cur="${COMP_WORDS[COMP_CWORD]}"
+	COMPREPLY=( $(compgen -W "$(docker images --format {{.Repository}}:{{.Tag}})" "$cur") )
+}
+complete -F _docker2tar docker2tar
+
 if [[ "$TERM" == "xterm-termite" ]]; then
 	alias docker="TERM=xterm-256color docker"
 	dbash() {
@@ -108,7 +120,7 @@ bwatch() {
 		eval output="\$($*)"
 		clear
 		echo "$output"
-		sleep 1
+		sleep 0.5
 	done
 }
 
