5ee583992bbf4dc05f4f152094838796937fa724
GinjaNinja32 <ginjaninja32@gmail.com>
2017-09-22 17:56:30 +0100

    Set DP-2 as primary monitor on tartarus

--- layouts/tartarus-HV.sh
+++ layouts/tartarus-HV.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-5 --off --output DP-4 --mode 3840x2160 --pos 3840x0 --rotate right --output DP-3 --off --output DP-2 --mode 3840x2160 --pos 0x890 --rotate normal --output DP-1 --off --output DP-0 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-5 --off --output DP-4 --mode 3840x2160 --pos 3840x0 --rotate right --output DP-3 --off --output DP-2 --primary --mode 3840x2160 --pos 0x890 --rotate normal --output DP-1 --off --output DP-0 --off
