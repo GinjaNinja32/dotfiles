dbaad38b7c9637b3e839e0d1374a22dea763fe7d
GinjaNinja32 <ginjaninja32@gmail.com>
2019-03-21 11:09:37 +0000

    updates

--- bashrc/env.sh
+++ bashrc/env.sh
@@ -5,3 +5,5 @@ export EDITOR=vim
 _pathprepend "$HOME/.local/bin"
 _pathprepend "$HOME/bin" "$HOME/dotfiles/scripts" "$HOME/dotfiles/scripts/steam"
 
+[[ -e /usr/local/go ]] && _pathappend /usr/local/go/bin
+[[ -e "$HOME/.cargo/bin" ]] && _pathappend "$HOME/.cargo/bin"
