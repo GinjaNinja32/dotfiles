61df037a5a4f7315a7f330b38515fe448868ffd3
Ben Paxton <ben.paxton@headforwards.com>
2022-02-16 14:08:49 +0000

    Adjust monitor offset to more closely match actual position

--- layouts/bp-ubuntu-home-HV.sh
+++ layouts/bp-ubuntu-home-HV.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --mode 3840x2160 --pos 0x784 --rotate normal --output HDMI-1 --primary --mode 3840x2160 --pos 3840x0 --rotate right
+xrandr --output HDMI-2 --mode 3840x2160 --pos 0x900 --rotate normal --output HDMI-1 --primary --mode 3840x2160 --pos 3840x0 --rotate right
